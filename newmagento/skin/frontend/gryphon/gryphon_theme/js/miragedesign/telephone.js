efcc57ce15e5095cc372f9afa4b455bccb2c5f77
zawzawzaw <zawzawzaw@gmail.com>
2015-07-20 17:37:01 +0800

    updated

--- newmagento/skin/frontend/gryphon/gryphon_theme/js/miragedesign/telephone.js
+++ newmagento/skin/frontend/gryphon/gryphon_theme/js/miragedesign/telephone.js
@@ -1,7 +1,16 @@
 (function($){
     $(function() {
-        $("#telephone").intlTelInput({
-            utilsScript: "lib/intl-tel-input/lib/libphonenumber/build/utils.js"
-        });
+        // $("#phone_no").intlTelInput({
+        //     // utilsScript: "lib/intl-tel-input/lib/libphonenumber/build/utils.js"
+        // });
+    	$("#telephone").intlTelInput({
+    		utilsScript: "lib/intl-tel-input/lib/libphonenumber/build/utils.js"
+    	});
+    	$("#phone_no").intlTelInput({
+	        defaultCountry: 'sg',
+	        nationalMode: false,
+	        autoHideDialCode: false,
+	        autoPlaceholder: false
+	      });
     });
 })(jQuery);
